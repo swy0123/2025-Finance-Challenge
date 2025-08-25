e36c0c84bf311e0b228f3a67065f0cdfc560267d
이시영 <swy05123@gmail.com>
2025-08-26 00:12:34 +0900

    빌드 오류 수정 및 초기화면 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 // app/page.tsx
-export default function HomePage() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Crypto Dashboard</h1>
-      <ul>
-        <li><a href="/transfer">스테이블 코인 송금/거래 시뮬레이터</a></li>
-        <li><a href="/prices">코인 시세</a></li>
-        <li><a href="/rates">환율</a></li>
-        <li><a href="/reports">분석 보고서</a></li>
-      </ul>
-    </div>
-  );
+// export default function HomePage() {
+//   return (
+//     <div style={{ padding: 20 }}>
+//       <h1>Crypto Dashboard</h1>
+//       <ul>
+//         <li><a href="/transfer">스테이블 코인 송금/거래 시뮬레이터</a></li>
+//         <li><a href="/prices">코인 시세</a></li>
+//         <li><a href="/rates">환율</a></li>
+//       </ul>
+//     </div>
+//   );
+// }
+import { redirect } from "next/navigation";
+export default function Home() {
+  redirect("/transfer");
 }
